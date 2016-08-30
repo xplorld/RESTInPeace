442018229be3f487aec5626c7a8c3c8e2a980bee
xplorld <xplorld@gmail.com>
2016-08-30 18:59:31 +0800

    Initial Commit

---
+++ RESTInPeace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RESTInPeace
+//
+//  Created by Xplorld on 2016/8/30.
+//  Copyright © 2016年 xplorld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
